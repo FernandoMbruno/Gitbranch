e2f74cde46c076903c64339a3e676bbeda0364d1
FernandoMBruno <fernando.bruno@evosystems.com.br>
2021-11-19 11:18:33 -0300

    Initial Commit

---
+++ teste/testeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeApp.swift
+//  teste
+//
+//  Created by EvoMacBookM1-001 on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct testeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
